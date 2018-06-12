a2f1169ce75524482842fd99d4267890e7674cc6
James Blakeborough <JamesB435@hotmail.com>
2018-06-12 21:15:48 +0100

    Beautified Code

--- src/ReactComponents/ShelfSelecter.js
+++ src/ReactComponents/ShelfSelecter.js
@@ -11,14 +11,18 @@ class ShelfSelector extends Component {
     return (
 
             <div className="book-shelf-changer">
+
                                 <select value = {this.props.book.shelf} onChange={(event) => this.props.shelfChange(this.props.book, event.target.value)}>
+
                                 <option value="none" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
                                 <option value="none">None</option>
-                              </select>
-                            </div>
+                                
+                                </select>
+
+            </div>
       )
   }
 }
