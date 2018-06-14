799c30a3701f55b121550a44b18fc35d4af9a9e2
James Blakeborough <JamesB435@hotmail.com>
2018-06-14 19:49:16 +0100

    Beautification.

--- src/ReactComponents/Books.js
+++ src/ReactComponents/Books.js
@@ -7,25 +7,18 @@ class Book extends Component {
 
     return(
       <li>
-
         <div className="book">
-
           	<div className="book-top">
-
-            	<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.img})` }}></div>
-
-				<ShelfSelector shelf = {this.props.shelf}
-				book ={this.props.book}
-				shelfChange={this.props.shelfChange}
-        select value = {this.props.shelf}/>
-
-          	</div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.img})` }}></div>
+				            <ShelfSelector shelf = {this.props.shelf}
+				            book ={this.props.book}
+				            shelfChange={this.props.shelfChange}
+                    select value = {this.props.shelf}/>
+          	    </div>
 
           		<div className="book-title">{this.props.title}</div>
           		<div className="book-authors">{this.props.author}</div>
-
         </div>
-
       </li>
     )
   }
