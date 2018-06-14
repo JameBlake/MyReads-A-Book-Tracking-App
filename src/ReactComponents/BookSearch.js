ee7c371cb3c1e0702ac1b82dbc1524e5d63bc243
James Blakeborough <JamesB435@hotmail.com>
2018-06-14 19:47:53 +0100

    Updated code to reflect what shelves the books are on in the search.

--- src/ReactComponents/BookSearch.js
+++ src/ReactComponents/BookSearch.js
@@ -11,20 +11,16 @@ class BookSearch extends Component {
           booksFound:[]
 }
 
-
 updateQuery = (event) => {
     const query = event;
     if (query) {
       this.setState({query})
       BooksAPI.search(query).then( enquire => {
           if (enquire instanceof Array) {
-
             const found = enquire.filter(found => found.authors && found.imageLinks);
             this.addShelf(found)
             this.setState({ booksFound: found });
             console.log(this.state.booksFound);
-            
-
         } else {
           this.setState({ booksFound: [] })
         }
@@ -35,7 +31,6 @@ updateQuery = (event) => {
     }
   }
 
-
 addShelf(found) {
 	if(found){
 		found.map(find => {
@@ -43,62 +38,52 @@ addShelf(found) {
 			return find;
 		})
 	} else return
- }
+}
 
 shelving(find){
-
-let hasShelf = this.props.books.filter(book => book.id === find.id);
-    return hasShelf.length ? hasShelf[0].shelf : "none";
+let onShelf = this.props.books.filter(book => book.id === find.id);
+    return onShelf.length ? onShelf[0].shelf :'inLibrary';
 }
 
 render(){
 
   return (
-
     <div className="search-books">
-
             <div className="search-books-bar">
-
                     <Link className="close-search" to ="/">Close</Link>
-
                     <div className="search-books-input-wrapper">
-                      <input type="text" placeholder="Search by title or author" value = {this.state.query} onChange = {(event) => this.updateQuery(event.target.value)}/>
+                      	<input type="text" placeholder="Search by title or author"
+                      	value = {this.state.query}
+                      	onChange = {(event) => this.updateQuery(event.target.value)}/>
                     </div>
-
             </div>
-
             <div className="search-books-results">
-            
-                      <ol className="books-grid">
+                    <ol className="books-grid">
+                    {this.state.query && this.state.booksFound.map((book)=>(
+                    <Book book={book}
+                    title={book.title}
+                    author={book.authors}
+                    img={book.imageLinks.thumbnail}
+                    key = {book.id}
+                    books={this.props.books}
+                    shelf={book.shelf}
+                    shelfChange={this.props.shelfChange}
+                    />))}
+                    </ol>
 
-                          {this.state.query && this.state.booksFound.map((book)=>(
-                          <Book book={book}
-                          title={book.title}
-                          author={book.authors}
-                          img={book.imageLinks.thumbnail}
-                          key = {book.id}
-                          books={this.props.books}
-                          shelf={book.shelf}
-                          shelfChange={this.props.shelfChange}
-                          />))}
+                    {this.state.query.length === 0 && (
+                    <div>
+                        <div className = "Welcome-to-the-search-page.">
+                               <h3> Welcome! I hope you can find what you are looking for.</h3>
+                         </div>
+                    </div>)}
 
-                        </ol>
-
-                          {this.state.query.length === 0 && (
-                          <div>
-                              <div className = "Welcome-to-the-search-page.">
-                                    <h3> Welcome! I hope you can find what you are looking for.</h3>
-                              </div>
-                          </div>
-                          )}
-
-                          {this.state.query.length >= 2 && this.state.booksFound.length === 0 && (
-                          <div>
-                              <div className ="Sorry">
-                                    <h3> Unfortunately we don't have what you are looking for. Please try again.</h3>
-                              </div>
-                          </div>
-                          )}
+                    {this.state.query.length >= 2 && this.state.booksFound.length === 0 && (
+                    <div>
+                        <div className ="Sorry">
+                            	<h3> Unfortunately we don't have what you are looking for. Please try again.</h3>
+                        </div>
+            		</div> )}
             </div>
     </div>
       )
