70b7e9f4b1b40f3eafd1136a462549bd82a69e9d
James Blakeborough <JamesB435@hotmail.com>
2018-06-14 19:50:39 +0100

    Small change to select option.

--- src/ReactComponents/ShelfSelecter.js
+++ src/ReactComponents/ShelfSelecter.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react'
 
 
-
 class ShelfSelector extends Component {
-
-
 
   render(){
 
     return (
 
             <div className="book-shelf-changer">
-
-                                <select value = {this.props.book.shelf} onChange={(event) => this.props.shelfChange(this.props.book, event.target.value)}>
-
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                                
-                                </select>
-
+                    <select value = {this.props.book.shelf} onChange={(event) => this.props.shelfChange(this.props.book, event.target.value)}>
+                    <option value="none" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="inLibrary">None</option>
+                    </select>
             </div>
       )
   }
